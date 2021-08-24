85947ac9f6c67bb7169ba0a2147fab244e5f396d
Arthur Primo Graciano <arthur_graciano@hotmail.com>
2021-08-23 21:54:19 -0300

    Mecanicas
    Movimentação do player
    Começo da mecanica de abdução

--- Brackeys2021.2/Assets/Scripts/PlayerController.cs
+++ Brackeys2021.2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody playerRB;
+    public float moveSpeed;
+
+    private Vector2 moveInput;
+
+    private void Start()
     {
+        playerRB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        moveInput.Normalize();
+
+        playerRB.velocity = new Vector3
+            (moveInput.x * moveSpeed, playerRB.velocity.y, moveInput.y * moveSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AbductionRaycast();
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void AbductionRaycast()
     {
-        
+        Vector3 origin = transform.position;
+        Vector3 direction = Vector3.down;
+
+        Debug.DrawRay(origin, direction * moveSpeed, Color.red);
+        Ray ray = new Ray(origin, direction);
+
+        if(Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            if(raycastHit.transform.tag == "Target")
+            {
+                Vector3 targetPos = raycastHit.transform.position;
+                raycastHit.transform.position = Vector3.Lerp(targetPos, origin, 5f * Time.deltaTime);
+            }
+        }
     }
+
 }
